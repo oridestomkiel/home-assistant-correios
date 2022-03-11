9963191af327ce5d65b29cf1c9fae44240d80fa0
Orides Tomkiel Zmovirzynski <orides.tomkiel@totvs.com.br>
2022-03-11 20:14:31 -0300

    Add custom components folder and base files

---
+++ custom_components/correios/__init__.py
@@ -0,0 +1,5 @@
+"""
+Home Assistant platform that provides information about the tracking of objects in the post office in Brazil.
+
+https://github.com/oridestomkiel/home-assistant-correios
+"""
---
+++ custom_components/correios/sensor.py
@@ -0,0 +1,6 @@
+"""
+A platform that provides information about the tracking of objects in the post office in Brazil
+
+For more details about this component, please refer to the documentation at
+https://github.com/oridestomkiel/home-assistant-correios
+"""
