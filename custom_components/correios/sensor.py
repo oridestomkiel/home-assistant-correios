4a5da115a558fc3ba49f891f086187bca0ae161d
dougiteixeira <31328123+dougiteixeira@users.noreply.github.com>
2022-03-15 23:37:41 -0300

    Adiciona fluxo de configuração

--- custom_components/correios/sensor.py
+++ custom_components/correios/sensor.py
@@ -6,44 +6,64 @@ https://github.com/oridestomkiel/home-assistant-correios
 
 import logging
 import async_timeout
-import voluptuous as vol
-import homeassistant.helpers.config_validation as cv
+from homeassistant.components.sensor import SensorEntity
 from homeassistant.helpers.aiohttp_client import async_create_clientsession
-from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.helpers.entity import Entity
-from typing import Final
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers import device_registry as dr
+
 import json
-
-CONF_TRACKING = 'track'
-CONF_DESCRIPTION = 'description'
-DEFAULT_DESCRIPTION: Final = "Encomenda"
-
-ICON = 'mdi:box-variant-closed'
-BASE_API = 'https://proxyapp.correios.com.br/v1/sro-rastro/{}'
-BASE_URL = 'https://proxyapp.correios.com.br'
-
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_TRACKING): cv.string,
-    vol.Optional(CONF_DESCRIPTION, default=DEFAULT_DESCRIPTION): cv.string,
-})
+from .const import (
+    BASE_API,
+    BASE_URL,
+    CONF_TRACKING,
+    CONF_DESCRIPTION,
+    DOMAIN,
+    ICON,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
 
-async def async_setup_platform(
-        hass, config, async_add_entities, discovery_info=None):
-
-    track = config['track']
-    description = config['description']
+async def async_setup_entry(
+    hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
+) -> None:
+    """Set up Tuya sensor dynamically through Tuya discovery."""
+    track = entry.data[CONF_TRACKING]
+    description = entry.data[CONF_DESCRIPTION]
     session = async_create_clientsession(hass)
-    name = "CR " + track
+    name = f"{description} ({track})"
+
+    device_registry = dr.async_get(hass)
+
+    device_registry.async_get_or_create(
+        config_entry_id=entry.entry_id,
+        connections=None,
+        identifiers={(DOMAIN, track)},
+        manufacturer="Correios",
+        name=track,
+        model="Não aplicável",
+        sw_version=None,
+        hw_version=None,
+    )
+
     async_add_entities(
-        [CorreiosSensor(track, name, description, session)], True)
+        [CorreiosSensor(track, entry.entry_id, name, description, session)],
+        True,
+    )
 
 
-class CorreiosSensor(Entity):
-
-    def __init__(self, track, name, description, session):
+class CorreiosSensor(SensorEntity):
+    def __init__(
+        self,
+        track,
+        config_entry_id,
+        name,
+        description,
+        session,
+    ):
         self.session = session
         self.track = track
         self._name = name
@@ -52,6 +72,7 @@ class CorreiosSensor(Entity):
         self.dtPrevista = None
         self.tipoPostal = None
         self._state = None
+        self._unique_id = track
         self.trackings = []
 
     async def async_update(self):
@@ -62,61 +83,85 @@ class CorreiosSensor(Entity):
                 info = await response.text()
                 obj_correio = json.loads(info)
                 self.trackings = []
-                if 'mensagem' in obj_correio['objetos'][0]:
-                    self._state = obj_correio['objetos'][0]['mensagem']
+                if "mensagem" in obj_correio["objetos"][0]:
+                    self._state = obj_correio["objetos"][0]["mensagem"]
                     self._image = "https://rastreamento.correios.com.br/static/rastreamento-internet/imgs/correios-sf.png"
                 else:
-                    if len(obj_correio['objetos'][0]['eventos']) > 0:
-                        self._state = obj_correio['objetos'][0]['eventos'][0]['descricao']
-                        for eventos in obj_correio['objetos'][0]['eventos']:
+                    if len(obj_correio["objetos"][0]["eventos"]) > 0:
+                        self._state = obj_correio["objetos"][0]["eventos"][0][
+                            "descricao"
+                        ]
+                        for eventos in obj_correio["objetos"][0]["eventos"]:
                             self.trackings.append(
-                                {'': '', "Descrição": eventos['descricao']})
-                            if 'unidadeDestino' in eventos:
+                                {"": "", "Descrição": eventos["descricao"]}
+                            )
+                            if "unidadeDestino" in eventos:
                                 self.trackings.append(
                                     {
-                                        "DE": eventos['unidade']['tipo'] + ", " + eventos['unidade']['endereco']['cidade'] + " - " + eventos['unidade']['endereco']['uf'],
-                                        "Para": eventos['unidadeDestino']['tipo'] + ", " + eventos['unidadeDestino']['endereco']['cidade'] + " - " + eventos['unidadeDestino']['endereco']['uf']
+                                        "DE": eventos["unidade"]["tipo"]
+                                        + ", "
+                                        + eventos["unidade"]["endereco"]["cidade"]
+                                        + " - "
+                                        + eventos["unidade"]["endereco"]["uf"],
+                                        "Para": eventos["unidadeDestino"]["tipo"]
+                                        + ", "
+                                        + eventos["unidadeDestino"]["endereco"][
+                                            "cidade"
+                                        ]
+                                        + " - "
+                                        + eventos["unidadeDestino"]["endereco"]["uf"],
                                     }
                                 )
                             else:
                                 self.trackings.append(
-                                    {"Em": eventos['unidade']['tipo'] + ", " + eventos['unidade']['endereco']['cidade'] + " - " + eventos['unidade']['endereco']['uf']})
-                            self.trackings.append({
-                                "Data/Hora": eventos["dtHrCriado"].replace("T", " ")})
-                    if 'dtPrevista' in obj_correio['objetos'][0]:
-                        self.dtPrevista = obj_correio['objetos'][0]['dtPrevista']
-                    self._image = BASE_URL + \
-                        obj_correio['objetos'][0]['eventos'][0]['urlIcone']
+                                    {
+                                        "Em": eventos["unidade"]["tipo"]
+                                        + ", "
+                                        + eventos["unidade"]["endereco"]["cidade"]
+                                        + " - "
+                                        + eventos["unidade"]["endereco"]["uf"]
+                                    }
+                                )
+                            self.trackings.append(
+                                {"Data/Hora": eventos["dtHrCriado"].replace("T", " ")}
+                            )
+                    if "dtPrevista" in obj_correio["objetos"][0]:
+                        self.dtPrevista = obj_correio["objetos"][0]["dtPrevista"]
+                    self._image = (
+                        BASE_URL + obj_correio["objetos"][0]["eventos"][0]["urlIcone"]
+                    )
                     self.info = info
-                    self.tipoPostal = obj_correio['objetos'][0]['tipoPostal']['categoria'] + \
-                        " - " + \
-                        obj_correio['objetos'][0]['tipoPostal']['descricao']
+                    self.tipoPostal = (
+                        obj_correio["objetos"][0]["tipoPostal"]["categoria"]
+                        + " - "
+                        + obj_correio["objetos"][0]["tipoPostal"]["descricao"]
+                    )
         except Exception as error:
-            _LOGGER.error('%s - Não foi possível atualizar - %s',
-                          self.info, error)
+            _LOGGER.error("%s - Não foi possível atualizar - %s", self.info, error)
 
-    @ property
+    @property
     def name(self):
         return self._name
 
-    @ property
+    @property
     def entity_picture(self):
         return self._image
 
-    @ property
+    @property
     def state(self):
         return self._state
 
-    @ property
+    @property
     def icon(self):
         return ICON
 
-    @ property
+    @property
     def extra_state_attributes(self):
 
-        return {'Descrição': self.description,
-                'Código Objeto': self.track,
-                'Data Prevista': self.dtPrevista,
-                'Tipo Postal': self.tipoPostal,
-                'Movimentações': self.trackings
-                }
+        return {
+            "Descrição": self.description,
+            "Código Objeto": self.track,
+            "Data Prevista": self.dtPrevista,
+            "Tipo Postal": self.tipoPostal,
+            "Movimentações": self.trackings,
+        }
